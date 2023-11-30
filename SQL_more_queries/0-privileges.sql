177b8f8ed34f5f9fa9f82a56f954fcc07d6e0696
Belinda1704 <b.larose@alustudent.com>
2023-11-29 16:46:51 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- script 0
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on
--- your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show privileges
+-- Show privileges for `user_0d_1`
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Show privileges for `user_0d_2`
+SHOW GRANTS FOR user_0d_2@localhost;
