6eb606a3370549bf2d824583e1aa76daba0ad80b
Belinda1704 <b.larose@alustudent.com>
2023-12-01 12:37:39 -0800

    Python script that fetches url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
-from urllib.error import HTTPError
+"""
+Module 0-hbtn-status
+Fetches https://intranet.hbtn.io/status
+"""
 
-if __name__ == "__main__":
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-            r = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(r)))
-            print("\t- content: {}".format(r))
-            print("\t- utf8 content: {}".format(r.decode("UTF-8")))
-    except HTTPError as e:
-        print("Error: {}".format(e))
+
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:')
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
