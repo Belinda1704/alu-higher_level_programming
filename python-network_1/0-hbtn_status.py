88b13862de8fe8a50726b5f61f8146304aa21e16
Belinda1704 <b.larose@alustudent.com>
2023-12-01 07:55:02 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""This script fetches data from
-    https://alx-intranet.hbtn.io/status
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
 
-import urllib.request
 
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
--- python-network_1/start_web_server.sh
+++ python-network_1/start_web_server.sh
@@ -1,19 +1,1 @@
 
-t]
-Body response:
-	- type: <class 'bytes'>
-	- content: b'OK'
-	- utf8 content: OK
-
-(78 chars long)
-[stderr]: 
-(0 chars long)
-[Expected]
-Body response:
-	- type: <class 'bytes'>
-	- content: b'Custom status'
-	- utf8 content: Custom status
-
-(100 chars long)
-[stderr]: [Anything]
-(0 chars long)
